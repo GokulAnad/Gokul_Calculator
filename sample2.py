40cb1fb379ef946def5a56997878603db13b0f91
GokulAnad <fooddynatural@gmail.com>
2021-10-13 02:58:07 +0530

    UI design complited

--- sample2.py
+++ sample2.py
@@ -1,11 +1,19 @@
 import tkinter
 from tkinter import *
+import tkinter.font as font
 
 m = Tk()
+m.title("Gokul Calculator")
+m.geometry("435x400")
 def display():
     print("hello")
 
-Text=Entry(m)
+Text=Entry(m ,font ="Helvetica 20 bold",justify ="right")
+#m.columnconfigure(0,weight=2)
+#m.columnconfigure(1,weight=2)
+m.rowconfigure(1,weight =1)
+myFont = font.Font(family='Helvetica 30 bold')
+tkinter.Button(m,font ="Helvetica 30 bold")
 
 buttonClear= tkinter.Button(m ,text="C",width=10,height =3,command = display())
 buttonDiv = tkinter.Button(m ,text="/",width=10,height =3,command = display())
@@ -22,13 +30,16 @@ buttonSub = tkinter.Button(m ,text="-",width=10,height =3,command = display())
 button4 = tkinter.Button(m ,text="4",width=10,height =3,command = display())
 button5 = tkinter.Button(m ,text="5",width=10,height =3,command = display())
 button6 = tkinter.Button(m ,text="6",width=10,height =3,command = display())
+buttonDel = tkinter.Button(m ,text="Del",width=10,height =3,command = display())
 
 button1 = tkinter.Button(m ,text="1",width=10,height =3,command = display())
 button2 = tkinter.Button(m ,text="2",width=10,height =3,command = display())
 button3 = tkinter.Button(m ,text="3",width=10,height =3,command = display())
 buttonEqual = tkinter.Button(m ,text="=",width=10,height =3,command = display())
 
-Text.grid(row = 1,sticky =W)
+Text.grid(row = 1,columnspan=4,sticky ='ns,ew')
+
+
 
 buttonClear.grid(row=2,column=0 ,pady =2)
 buttonDiv.grid(row=2,column=1 ,pady =2)
@@ -43,6 +54,7 @@ buttonSub.grid(row=3,column=3 ,pady =2)
 button4.grid(row=4,column=0 ,pady =2)
 button5.grid(row=4,column=1 ,pady =2)
 button6.grid(row=4,column=2 ,pady =2)
+buttonDel.grid(row=4,column=3 ,pady =2)
 
 button1.grid(row=5,column=0 ,pady =2)
 button2.grid(row=5,column=1 ,pady =2)
